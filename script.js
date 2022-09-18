3fe8c15b34a7c53fef83d62bb25b6d5b806611b4
Dmitriy Meshechkov <d.meshechkov@yandex.ru>
2022-09-18 20:47:16 +0500

    Modal window

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-'use strict';
+"use strict";
+
+const modalWindow = document.querySelector(".modal-window");
+const overlay = document.querySelector(".overlay");
+const btnCloseModalWindow = document.querySelector(".close-modal-window");
+const btnShowModalWindow = document.querySelectorAll(".show-modal-window");
+console.log(btnShowModalWindow);
+
+const showModalWindow = function () {
+  modalWindow.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModalWindow = function () {
+  modalWindow.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnShowModalWindow.length; i++) {
+  btnShowModalWindow[i].addEventListener("click", showModalWindow);
+}
+
+btnCloseModalWindow.addEventListener("click", closeModalWindow);
+
+overlay.addEventListener("click", closeModalWindow);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modalWindow.classList.contains("hidden")) {
+    closeModalWindow();
+  }
+});
